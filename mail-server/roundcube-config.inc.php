59bab0e9307a18c89a611127362261aa4d0683d9
Carlos Cativo <cativo23.kt@gmail.com>
2026-03-24 18:17:59 -0600

    fix(mail): mount roundcube config file as volume for plain IMAP

---
+++ mail-server/roundcube-config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Roundcube config for docker-mailserver
+// Traefik handles SSL for web, internal traffic is plain
+$config['plugins'] = [];
+$config['imap_host'] = 'mail:143';
+$config['smtp_host'] = 'mail:587';
+$config['smtp_port'] = 587;
+$config['db_dsnw'] = 'sqlite:////var/roundcube/db/db.sqlite';
+$config['skin'] = 'elastic';
