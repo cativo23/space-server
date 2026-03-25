f8a3f55ca8affe3bbe06967931c532e22cd7dd1e
Carlos Cativo <cativo23.kt@gmail.com>
2026-03-24 20:11:04 -0600

    fix(mail): configure Roundcube SMTP authentication with user credentials

--- mail-server/roundcube-config.inc.php
+++ mail-server/roundcube-config.inc.php
@@ -5,5 +5,7 @@ $config['plugins'] = [];
 $config['imap_host'] = 'mail:143';
 $config['smtp_host'] = 'mail:587';
 $config['smtp_port'] = 587;
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
 $config['db_dsnw'] = 'sqlite:////var/roundcube/db/db.sqlite';
 $config['skin'] = 'elastic';
