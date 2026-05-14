950c208f5b0f9bb2f5510ea5c5ba35d7ba8014c3
Carlos Cativo <cativo23.kt@gmail.com>
2026-05-14 17:03:30 -0600

    chore(mail): sync Roundcube config to production state
    The committed version was missing the TLS settings polaris2 has had
    for weeks. Bring the repo in line so a fresh deploy reproduces the
    working webmail config.

--- mail-server/roundcube-config.inc.php
+++ mail-server/roundcube-config.inc.php
@@ -1,6 +1,4 @@
 <?php
-// Roundcube config for docker-mailserver
-// Traefik handles SSL for web, internal traffic is plain
 $config['plugins'] = [];
 $config['imap_host'] = 'mail:143';
 $config['smtp_host'] = 'mail:587';
@@ -8,5 +6,11 @@ $config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['smtp_auth_type'] = 'PLAIN';
+$config['smtp_use_tls'] = true;
+$config['smtp_tls_wrapper'] = false;
 $config['db_dsnw'] = 'sqlite:////var/roundcube/db/db.sqlite';
 $config['skin'] = 'elastic';
+$config['imap_conn_options']['ssl']['verify_peer'] = false;
+$config['imap_conn_options']['ssl']['verify_peer_name'] = false;
+$config['smtp_conn_options']['ssl']['verify_peer'] = false;
+$config['smtp_conn_options']['ssl']['verify_peer_name'] = false;
